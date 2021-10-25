d81f75b51c147643a052441d84d0d60e5cfa8816
pdisi <pdisidoro@yahoo.com>
2021-10-24 19:40:14 -1000

    completed assessment 6

--- rails-commenting.rb
+++ rails-commenting.rb
@@ -7,24 +7,30 @@
 # FILE: app/controller/blog_posts_controller.rb
 
 # ---1)
+#According to the syllabus this is where the "shape" of the database comes into fruition. I am looking at the class Dog < ApplicationRecord example. For this one ApplicationController is now directly connected to the class and can be called up on separately.
 class BlogPostsController < ApplicationController
+  #All of these are mostly setting up CRUD actions.
   def index
     # ---2)
+    #This .all will show everything that falls under BlogPost. I believe this is the same functionality as SELECT * FROM x in SQL. This is establishing the class index.
     @posts = BlogPost.all
   end
 
   def show
     # ---3)
+    #using find with the parameter of :id will specifically find and select an object that the user wants to work with.
     @post = BlogPost.find(params[:id])
   end
 
   # ---4)
+  # Creates a  new model Post, which can then be saved based on given parameters.
   def new
     @post = Post.new
   end
 
   def create
     # ---5)
+    # We can use the .create method to create and save comments. It automatically links a comment to the given post. Then it sends the user back to the post_path that the redirect is indicating.
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -34,6 +40,7 @@ class BlogPostsController < ApplicationController
   end
 
   # ---6)
+  #find method is searching for a specific paramter [:id] then we are passing in an update class.
   def edit
     @post = BlogPost.find(params[:id])
   end
@@ -41,6 +48,7 @@ class BlogPostsController < ApplicationController
   def update
     @post = BlogPost.find(params[:id])
     # ---7)
+    #This is how we can edit the blog post params and give them a function. Here we are using conditionals to redirect the user to the proper location. It also includes the destroy class that is redirecting the user to blog_posts_path if the parameter they are working in is destroyed.
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -55,14 +63,19 @@ class BlogPostsController < ApplicationController
       redirect_to blog_posts_path
     else
       # ---8)
+      #I believe this just redirects the user to the specified path after the condition of not destroying is met.
       redirect_to blog_post_path(@post)
     end
   end
 
   # ---9)
+  #I believe private is an extra security layer that prevents potential threats from modifying the established code.
   private
   def blog_post_params
     # ---10)
+    #I think these are the requirements for modification within the private settings. Having difficulty with verifying this.
     params.require(:blog_post).permit(:title, :content)
   end
 end
+
+#Notes on process: This whole week on Rails has been difficult for me because I am struggling with keeping interested on it. My appreciation for Javascript and React has grown significantly from this experience, so I am appreciative of that. I will attempt to give Rails more of my focus as I wouldn't want to close any doors that could potentially be enjoyable later on.
